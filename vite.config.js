fe774d675728cbe3f9dc3801deff49391e7f1090
Gabriel <gabrielgoldonif@gmail.com>
2026-01-12 10:33:09 -0300

    configura deploy automatico no github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/ci-cd-front/",
   plugins: [react()],
   test: {
     globals: true,
